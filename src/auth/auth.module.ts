1bb78cc062b7117160224f72f153b538b1f855f4
avinassh6vital <avinash.kolluru@caremonitor.com.au>
2026-06-11 21:52:59 +0530

    fix: increase JWT expiration time from 60s to 6000s in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { jwtConstants } from '../database/config';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '6000s' },
     }),
   ],
 })
