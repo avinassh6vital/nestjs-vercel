0f049a5bed639f02c446270191aefb9064ac4ce0
avinassh6vital <avinash.kolluru@caremonitor.com.au>
2026-06-12 11:51:22 +0530

    fix: update app listener to bind to all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  //await app.listen(process.env.PORT ?? 3000);
+  // Listen on all network interfaces
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 } 
 void bootstrap();
